74c6d65721551bf76637be9a094ef5099cf5b704
Nada Omar <https://github.com/NadaOmar22>
2024-12-14 17:30:57 +0200

    Beans-Lifecycle

---
+++ Beans-Lifecycle/src/Practice/DatabaseOperations.java
@@ -0,0 +1,13 @@
+package Practice;
+
+public class DatabaseOperations {
+
+    void connectToDatabase(){
+        System.out.println("connection to database established ……");
+    }
+
+    void disconnectFromDatabase (){
+        System.out.println("connection to database closed……");
+    }
+
+}
